3644364a1a69d04c9d2425a4044f37739779978c
Khalil-Guetari <37622394+Khalil-Guetari@users.noreply.github.com>
2018-10-10 10:20:32 +0200

    Add files via upload

---
+++ FrontofficeHomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Circuit;
+use App\Repository\CircuitRepository;
+
+class FrontofficeHomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="frontoffice_home")
+     */
+    public function index()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $circuits = $em->getRepository(Circuit::class)->findAll();
+        
+        dump($circuits);
+        
+        return $this->render('front/home.html.twig', array(
+            'circuits' => $circuits,
+        ));
+    }
+    /**
+     * Finds and displays a circuit entity.
+     *
+     * @Route("/circuit/{id}", name="front_circuit_show")
+     */
+    public function circuitShow($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $circuit = $em->getRepository(Circuit::class)->find($id);
+        
+        dump($circuit);
+        
+        return $this->render('front/circuit_show.html.twig', [
+            'circuit' => $circuit,
+        ]);
+    }
+}
